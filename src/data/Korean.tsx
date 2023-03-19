cac326353ad56c3ec0f94274ad66120d606ca34b
Kev <sulpeum@gmail.com>
2023-03-19 16:27:38 -0400

    add: typed the alphabet array

--- src/data/Korean.tsx
+++ src/data/Korean.tsx
@@ -1,5 +1,14 @@
-export const KoreanAlphabet = [
+export type Alphabet = {
+  id: number
+  english: string
+  korean: string
+  memorized: boolean
+}
+
+export const KoreanAlphabet: Alphabet[] = [
   { id: 0, english: "n", korean: "ㄴ", memorized: false },
   { id: 1, english: "g", korean: "ㄱ", memorized: false },
   { id: 2, english: "d", korean: "ㄷ", memorized: false },
+  { id: 3, english: "h", korean: "ㅎ", memorized: false },
+  { id: 4, english: "b", korean: "ㅂ", memorized: false },
 ]
